3dc3c9ff43588779b1578766ae8fced0ef35304a
unknown <tjtik83@gmail.com>
2022-01-11 06:45:14 +0700

    add _Layout

--- FirstMVCCoreApp/Startup.cs
+++ FirstMVCCoreApp/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,6 +25,8 @@ namespace FirstMVCCoreApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            //services.AddRazorPages().AddRazorOptions(options => options.ad);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +41,10 @@ namespace FirstMVCCoreApp
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseStaticFiles();
+            //app.UseStaticFiles(new StaticFileOptions() { 
+            //    FileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory()+ "MyStaticFiles"), 
+            //    RequestPath = "/MyStaticFiles"
+            //});;
 
             app.UseRouting();
 
